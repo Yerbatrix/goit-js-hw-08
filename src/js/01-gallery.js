955d4c175ac0984137fefb9de045c5f3e3188833
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-02-03 12:47:34 +0100

    task 01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,33 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-const a = 'Hello World';
-console.log(a);
+const fixedGalleryItems = galleryItems
+  .map(
+    galleryItem =>
+      `<div class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${galleryItem.preview}" 
+        alt="${galleryItem.description}"
+        data-source="${galleryItem.original}"
+        >
+        </img>
+        </a>
+     </div>`
+  )
+  .join('');
 
+gallery.insertAdjacentHTML('afterbegin', fixedGalleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 200,
+  captionPosition: 'bottom',
+});
 console.log(galleryItems);
