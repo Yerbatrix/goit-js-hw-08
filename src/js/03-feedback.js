017fd3619bf61dd3e9b70050ed9769878c681284
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-02-03 21:55:33 +0100

    task-3-argh

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -17,3 +17,31 @@ function saveInput() {
 
   console.log(dataToSend);
 }
+
+function onSubmit(event) {
+  event.preventDefault();
+  const sentData = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log(sentData);
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = null;
+  messageInput.value = null;
+}
+
+const throttleSave = throttle(saveInput, 500);
+
+email.addEventListener('input', throttleSave);
+message.addEventListener('input', throttleSave);
+form.addEventListener('submit', onSubmit);
+
+const parsedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (parsedData.email !== '' || parsedData.message !== '') {
+  // console.log('Local storage is NOT empty');
+  email.value = parsedData.email;
+  message.value = parsedData.message;
+} else {
+  // console.log('Local storage is empty');
+}
