fa393a054a825a33d86dd66f95de59b707b40a27
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-02-03 21:42:43 +0100

    task-03-trying

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
 
+const email = document.querySelector("[name='email']");
+const message = document.querySelector("[name='message']");
+const form = document.querySelector('.feedback-form');
+
+function saveInput() {
+  const email = email.value;
+  const message = message.value;
+  const formData = {
+    email,
+    message,
+  };
+
+  const dataToSend = JSON.stringify(formData);
+  localStorage.setItem('feedback-form-state', dataToSend);
+
+  console.log(dataToSend);
+}
