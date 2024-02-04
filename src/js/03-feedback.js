be316a63ae1677d4181ecd47087006d31219168f
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-02-04 09:33:47 +0100

    task-03-redo

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,15 @@
 import throttle from 'lodash.throttle';
 
-const email = document.querySelector("[name='email']");
-const message = document.querySelector("[name='message']");
+const email = form.querySelector("input[name='email']");
+const message = form.querySelector("textarea[name='message']");
 const form = document.querySelector('.feedback-form');
 
-function saveInput() {
-  const email = email.value;
-  const message = message.value;
-  const formData = {
-    email,
-    message,
-  };
-
-  const dataToSend = JSON.stringify(formData);
-  localStorage.setItem('feedback-form-state', dataToSend);
-
-  console.log(dataToSend);
-}
-
-function onSubmit(event) {
-  event.preventDefault();
-  const sentData = {
-    email: email.value,
-    message: message.value,
-  };
-  console.log(sentData);
-  localStorage.removeItem('feedback-form-state');
-  emailInput.value = null;
-  messageInput.value = null;
-}
-
-const throttleSave = throttle(saveInput, 500);
-
-email.addEventListener('input', throttleSave);
-message.addEventListener('input', throttleSave);
-form.addEventListener('submit', onSubmit);
-
-const parsedData = JSON.parse(localStorage.getItem('feedback-form-state'));
-
-if (parsedData.email !== '' || parsedData.message !== '') {
-  // console.log('Local storage is NOT empty');
-  email.value = parsedData.email;
-  message.value = parsedData.message;
-} else {
-  // console.log('Local storage is empty');
-}
+form.addEventListener(
+  'input',
+  throttle(() => {
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify({ email: email.value, message: message.value })
+    );
+  }, 500)
+);
